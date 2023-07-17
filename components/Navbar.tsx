4d8c97877e4e1d17ae3807faec9454776717a52b
anasss777 <anass2000271@gmail.com>
2023-07-17 15:43:03 +0100

    Search functionality on mobile.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,29 @@ import NavbarContentMobile from "./NavbarContentMobile";
 import { Category } from "@/types/Category";
 import { getCategories } from "@/sanity/sanity-utils";
 import NavbarContent from "./NavbarContent";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Navbar = () => {
   const [categories, setCategories] = useState<Category[]>([]);
+  // Search parameters
+  const search = useSearchParams();
+  const [searchQuery, setSearchQuery] = useState<string | null>(
+    search ? search.get("q") : ""
+  );
+  const router = useRouter();
+
+  // Search function
+  const handleSearch = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    if (typeof searchQuery !== "string") {
+      return;
+    }
+
+    const encodedSearchQuery = encodeURI(searchQuery);
+    router.push(`/search?q=${encodedSearchQuery}`);
+    setSearchQuery("");
+  };
 
   useEffect(() => {
     const fetchData = async () => {
@@ -33,20 +53,30 @@ const Navbar = () => {
       {/* For mobile */}
       <div className="flex flex-col relative md:hidden">
         {/* Search bar on mobile */}
-        <div className="bg-blue-300 w-full h-12 flex justify-center items-center md:hidden">
-          <Image
-            src={isOpen ? "/close.png" : "/menu.png"}
-            alt="Menu"
-            width={20}
-            height={20}
-            className="ml-3 invert"
-            onClick={() => setIsOpen(!isOpen)}
-          />
-          <input
-            className="w-full mx-3 rounded-full px-3 font-montserrat focus:border focus:border-blue-400 outline-none"
-            placeholder="Search..."
-          />
-        </div>
+        <form onSubmit={handleSearch}>
+          <div className="bg-blue-300 w-full h-12 flex justify-center items-center md:hidden">
+            <Image
+              src={isOpen ? "/close.png" : "/menu.png"}
+              alt="Menu"
+              width={20}
+              height={20}
+              className="ml-3 invert"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+            <input
+              className="w-full mx-3 rounded-full px-3 font-montserrat focus:border focus:border-blue-400 outline-none"
+              placeholder="Search..."
+              value={searchQuery || ""}
+              onChange={(event) => setSearchQuery(event.target.value)}
+            />
+            <button
+              className="bg-blue-500 float-right text-white font-montserrat px-3 font-bold rounded-full mr-3"
+              onClick={() => handleSearch}
+            >
+              Search
+            </button>
+          </div>
+        </form>
 
         {isOpen && (
           <div className="absolute top-12 flex flex-col w-56 md:hidden">
